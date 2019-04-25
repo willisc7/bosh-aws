ba5a87d4bb96aea2a5a9005fe74cbf16c5508aef
Christopher Willis <cwillis@CASKRMB0325s-MacBook-Pro.local>
2019-04-25 09:01:21 -0400

    added outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "public_subnet_id" {
+    value = "${aws_subnet.public.id}"
+}
+
+output "public_subnet_az" {
+    value = "${aws_subnet.public.availability_zone}"
+}
+
+output "public_subnet_region" {
+    value = "${var.aws_region}"
+}
